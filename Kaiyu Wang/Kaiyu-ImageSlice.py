5122c0350e93bf786f1fd4b8ae446dfe98fd7e1b
kaiyu <kwang22@cornellcollege.edu>
2020-10-25 21:47:34 -0500

    Kaiyu

---
+++ Kaiyu Wang/Kaiyu-ImageSlice.py
@@ -0,0 +1,25 @@
+# Import PIL module
+from PIL import Image
+
+# Import sys module for sys.argv(to get filename)
+import sys
+# Get filenames from argument
+def read_image(filename1, filename2):
+# Open the image files
+    image1 = Image.open(filename1)
+    image2 = Image.open(filename2)
+    width, height = image1.size[0], image1.size[1] # Get image size
+    image2 = image2.resize((width, height), Image.ANTIALIAS)
+
+# Save the output images
+    image1.crop((0, 0, width, height / 2)).save('output1a.jpg')
+    image1.crop((0, height / 2, width, height)).save('output1b.jpg')
+    image2.crop((0, 0, width, height / 2)).save('output2a.jpg')
+    image2.crop((0, height / 2, width, height)).save('output2b.jpg')
+
+
+
+
+
+
+
